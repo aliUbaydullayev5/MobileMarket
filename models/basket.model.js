e518d91c32a809ef8435972bb754be31345fcf4a
Ali <65356558+aliUbaydullayev5@users.noreply.github.com>
2022-11-05 17:14:51 +0500

    Update basket.model.js

--- models/basket.model.js
+++ models/basket.model.js
@@ -2,7 +2,10 @@ import {Schema, model, Types} from 'mongoose'
 
 const BasketSchema = new Schema({
     email: {type: String, required: true},
-    img: {type: String, required: true},
+    img1: {type: String, required: true},
+    img2: {type: String, required: true},
+    img3: {type: String, required: true},
+    img4: {type: String, required: true},
     title: {type: String, required: true},
     desc: {type: String, required: true},
     price: {type: String, required: true},
